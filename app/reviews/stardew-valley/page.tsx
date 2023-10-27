11de2d698c5942197f31efa1306801e3b794d1a7
Ray Olivier <rayolivier@outlook.com>
2023-10-27 14:16:01 -0500

    add md file for content and parse md in review page

--- app/reviews/stardew-valley/page.tsx
+++ app/reviews/stardew-valley/page.tsx
@@ -1,11 +1,20 @@
-import Heading from "@/components/Heading";
+import { readFile } from 'fs/promises';
+import matter from 'gray-matter';
+import { marked } from 'marked';
 
-export default function StardewValleyPage(){
-    return (
-        <>
-        <Heading>Stardew Valley</Heading>
-        <img src="/images/stardew-valley.jpg" alt="" width={640} height={360} className="font-semibold font-orbitron  mb-2 rounded" />
-        <p>Review goes here.</p>
-        </>
-    )
+import Heading from '@/components/Heading';
+
+export default async function StardewValleyPage() {
+	const text = await readFile('./content/reviews/stardew-valley.md', 'utf8'); // second param is character encoding
+    const {content, data: {title, date, image} } = matter(text);
+	const htmlContent = marked(content);
+
+	return (
+		<>
+			<Heading>{title}</Heading>
+            <p className='italic pb-2'>{date}</p>
+			<img src={image} alt="" width={640} height={360} className="font-semibold font-orbitron  mb-2 rounded" />
+			<article dangerouslySetInnerHTML={{ __html: htmlContent }} className='max-w-screen-sm prose prose-slate' />
+		</>
+	);
 }
