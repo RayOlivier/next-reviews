4e9430629c5af53a43aca3c2db7c70491b35aa59
Ray Olivier <rayolivier@outlook.com>
2023-10-31 14:57:39 -0500

    update about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,14 +2,17 @@ import Heading from '@/components/Heading';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'About',
+  title: 'About'
 };
 
 export default function AboutPage() {
   return (
     <>
       <Heading>About</Heading>
-      <p>about the site</p>
+      <p>
+        A simple project from the Udemy course &quotNext.js by Example&quot,
+        taught by Mirko Nasato.
+      </p>
     </>
   );
 }
