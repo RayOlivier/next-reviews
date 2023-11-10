ea1b44a20c7df51c6f10f318c841f532274fe890
Ray Olivier <rayolivier@outlook.com>
2023-11-10 12:19:28 -0600

    removing old code

--- app/page.tsx
+++ app/page.tsx
@@ -40,22 +40,6 @@ export default async function HomePage() {
           </li>
         ))}
       </ul>
-
-      {/* <div className="bg-white border rounded shadow w-80 sm:w-full hover:shadow-xl">
-        <Link
-          href="/reviews/stardew-valley"
-          className="flex flex-col sm:flex-row"
-        >
-          <img
-            src="/images/stardew-valley.jpg"
-            alt=""
-            width={640}
-            height={360}
-            className="rounded-t sm:rounded-l sm: rounded-r-none"
-          />
-          <h2 className="font-orbitron py-1 text-center">Stardew Valley</h2>
-        </Link>
-      </div> */}
     </>
   );
 }
